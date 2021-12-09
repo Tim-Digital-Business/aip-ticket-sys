b3eb27f524ea61f7e3230595f96da1b9672b807c
TimmiBoi <tim.digital.business@gmail.com>
2021-12-09 13:05:11 +0100

    Revision from Maél

--- faq.php
+++ faq.php
@@ -10,7 +10,7 @@
 
 
         <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="styles faq.css">
 
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -19,3 +19,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </script>
   </head>
+<body>
+  <h1>Willkommen bei unserem FAQ!</h1>
+</body>
