d04566ef41e7d194b2858b9ced55aecf3b259c07
GLACEONx <mael.schoch@gmx.ch>
2021-12-10 11:40:13 +0100

    no changes

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,11 @@
 function myFunction(img) {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    if (img.src.match("Light.png")){
-        img.src = "Dark.png";        
-    }
-    else {
+    if (img.src.match("Dark.png")){
         img.src = "Light.png";
+    }     
+    
+    else {
+        img.src = "Dark.png";
     }
- }
+}
