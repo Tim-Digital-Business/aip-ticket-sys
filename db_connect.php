fd7dc025bb8ed748d9498a90b07b95845933f762
GLACEONx <mael.schoch@gmx.ch>
2021-12-10 09:52:47 +0100

    test

--- db_connect.php
+++ db_connect.php
@@ -6,6 +6,4 @@ $user_pass = "";
 $database="timtest";
 
 #create DB connection
-$mysqli = new mysqli($host, $user,$user_pass, $database, 3306);
-
-?>
+$mysqli = new mysqli($host, $user,$user_pass, $database, 3306);?>
