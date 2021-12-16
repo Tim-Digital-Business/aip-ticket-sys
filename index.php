c8af39ce44f83102dca757d4a86170d2a25c1f34
TimmiBoi <tim.digital.business@gmail.com>
2021-12-16 10:47:07 +0100

    1

--- index.php
+++ index.php
@@ -1,28 +1,200 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
+<head>
+  <!-- Special import coz smothscrolling is picky -->
+  <title>AIP Plus</title>
+  <meta charset="UTF-8">
+  <link rel="shortcut icon" href="assets/images/AIPfavicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/parallax/jarallax.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap">
+  </noscript>
+  <link rel="preload" as="style" href="assets/css/mbr-additional.css">
+  <link rel="stylesheet" href="assets/css/mbr-additional.css" type="text/css">
+</head>
 
 <body>
-    <?php
-    include_once('navbar.php');
-    include_once('inc/head.php');
-    ?>
 
-    <main role="main" class="container">
+  <section data-bs-version="5.1" class="menu menu1 cid-sRc7rtfrMl" once="menu" id="menu1-k">
 
-        <div class="text-center mt-5 pt-5">
-            <h1>Digital Business</h1>
-            <p class="lead">Willkommen auf unserem Ticketverwaltungssystem</p>
+
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+      <div class="container">
+        <div class="navbar-brand">
+
+          <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-7" href="https://jsw.swiss">Jugensozialwerk</a></span>
         </div>
-
-        <div class="center">
-
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+            <li class="nav-item"><a class="nav-link link text-white display-4" href="../auth/login.php">Admin</a></li>
+          </ul>
         </div>
-        <div class="center">
-            <img class="img-fluid" src="/assets/home.jpg" height="1000" width="1500" style="border-radius: 100px">
-        </div>
+      </div>
+    </nav>
+  </section>
 
-    </main>
-    <?php include_once('inc/footer.php'); ?>
+  <section data-bs-version="5.1" class="header13 cid-sRc7vFQ6FN mbr-fullscreen mbr-parallax-background" id="header13-l">
+
+
+
+
+
+    <div class="align-center container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-lg-10">
+          <h1 class="text-white mbr-section-title mbr-fonts-style mb-3 display-1"><strong>Digital Business</strong></h1>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section data-bs-version="5.1" class="content17 cid-sRc7CKH74t" id="content17-n">
+    <div class="container">
+
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-10">
+          <div class="section-head align-center mb-4">
+            <h3 class="mbr-section-title mb-0 mbr-fonts-style display-2"><strong>FAQ</strong>
+            </h3>
+          </div>
+
+          <div id="bootstrap-toggle" class="toggle-panel accordionStyles tab-content">
+            <div class="card mb-3">
+              <div class="card-header" role="tab" id="headingOne">
+                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse1_2" aria-expanded="false" aria-controls="collapse1">
+                  <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>How to create more items in this list?</strong>
+                  </h6>
+                  <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                </a>
+              </div>
+              <div id="collapse1_2" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                  <p class="mbr-fonts-style panel-text display-7">The number of items is limited in this block. Open
+                    the Block parameters to change the amount of items.</p>
+                </div>
+              </div>
+            </div>
+            <div class="card mb-3">
+              <div class="card-header" role="tab" id="headingTwo">
+                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse2_2" aria-expanded="false" aria-controls="collapse2">
+                  <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>What is Mobirise Kit?</strong>
+                  </h6>
+                  <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                </a>
+
+              </div>
+              <div id="collapse2_2" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="panel-body">
+                  <p class="mbr-fonts-style panel-text display-7">Mobirise Kit is a service that provides the access to all current and new themes/extensions developed by Mobirise. New themes/extensions are released every month and will be available in your account during your plan period, without any additional charge.</p>
+                </div>
+              </div>
+            </div>
+            <div class="card mb-3">
+              <div class="card-header" role="tab" id="headingThree">
+                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse3_2" aria-expanded="false" aria-controls="collapse3">
+                  <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>Can I create sites for commerical projects?</strong>
+                  </h6>
+                  <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                </a>
+              </div>
+              <div id="collapse3_2" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <p class="mbr-fonts-style panel-text display-7">Yes, Mobirise is free for both non-profit and commercial sites.</p>
+                </div>
+              </div>
+            </div>
+            <div class="card mb-3">
+              <div class="card-header" role="tab" id="headingThree">
+                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse4_2" aria-expanded="false" aria-controls="collapse4">
+                  <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>What is Code Editor?</strong>
+                  </h6>
+                  <span class="sign mbr-iconfont mbri-arrow-down"></span>
+                </a>
+              </div>
+              <div id="collapse4_2" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <p class="mbr-fonts-style panel-text display-7">
+                    This extension allows editing the code of block in the app. Also, it's possible to add code to the head and body parts of pages.</p>
+                </div>
+              </div>
+            </div>
+            <div class="card mb-3">
+              <div class="card-header" role="tab" id="headingThree">
+                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse5_2" aria-expanded="false" aria-controls="collapse5">
+                  <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>What is Mobirise Kit?</strong></h6>
+                  <span class="mbr-iconfont mobi-mbri-arrow-down mobi-mbri"></span>
+                </a>
+              </div>
+              <div id="collapse5_2" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <p class="mbr-fonts-style panel-text display-7">
+                    Mobirise Kit is a service that provides the access to all current and new themes/extensions developed by Mobirise. New themes/extensions are released every month and will be available in your account during your plan period, without any additional charge.</p>
+                </div>
+              </div>
+            </div>
+            <div class="card mb-3">
+              <div class="card-header" role="tab" id="headingThree">
+                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-bs-toggle="collapse" data-core="" href="#collapse6_2" aria-expanded="false" aria-controls="collapse6">
+                  <h6 class="panel-title-edit mbr-fonts-style mb-0 display-7"><strong>Can I create sites for commerical projects?</strong></h6>
+                  <span class="mbr-iconfont mobi-mbri-arrow-down mobi-mbri"></span>
+                </a>
+              </div>
+              <div id="collapse6_2" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                  <p class="mbr-fonts-style panel-text display-7">
+                    Yes, Mobirise is free for both non-profit and commercial sites.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section data-bs-version="5.1" class="footer7 cid-sRc7MB4LCT mbr-reveal" once="footers" id="footer7-o">
+    <div class="container">
+      <div class="media-container-row align-center ">
+        <div class="col-12">
+          <p class="mbr-text mb-0 mbr-fonts-style display-8">
+            ©Copyright 2021 Jugensozialwerk - All Rights Reserved
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section style="background-color: #fff; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif; color:#aaa; font-size:12px; padding: 0; align-items: center; display: flex;">
+    <a href="https://mobirise.site/" style="visibility: hidden;"></a>
+    <p style="flex: 0 0 auto; margin:0; padding-right:1rem;">
+      <a href="" style="color:#aaa;"></a>
+    </p>
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/parallax/jarallax.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/ytplayer/index.js"></script>
+    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+    <script src="assets/mbr-switch-arrow/mbr-switch-arrow.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+  </section>
+
+
+
 </body>
 
 </html>
---
+++ index2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <?php
+    include_once('navbar.php');
+    include_once('inc/head.php');
+    ?>
+
+    <main role="main" class="container">
+
+        <div class="text-center mt-5 pt-5">
+            <h1>Digital Business</h1>
+            <p class="lead">Willkommen auf unserem Ticketverwaltungssystem</p>
+        </div>
+
+        <div class="center">
+
+        </div>
+        <div class="center">
+            <img class="img-fluid" src="/assets/home.jpg" height="1000" width="1500" style="border-radius: 100px">
+        </div>
+
+    </main>
+    <?php include_once('inc/footer.php'); ?>
+</body>
+
+</html>
